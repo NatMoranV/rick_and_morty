108ff205c2c7cda2e5aa123211aaca2a186410fa
NatMoranV <nat.moran.v@gmail.com>
2023-05-24 11:04:28 -0600

    cambios logo, ajustes css

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,7 @@
 import {SearchBar} from "../SearchBar/SearchBar";
 import { NavContainer } from "./StyledNav";
 import logo from '../../assets/img/logo.png'
+import logo2 from '../../assets/img/logo2.png'
 import { RandomButton } from "../RandomButton/StyledRandomButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShuffle } from "@fortawesome/free-solid-svg-icons";
@@ -15,7 +16,11 @@ export default function Nav ({onSearch}){
 
     return (
         <NavContainer>
+        <picture>
+        <source media="(max-width: 480px)" srcset={logo2}></source>
         <img id="logo" src={logo}/>
+        </picture>
+        
             <RandomButton onClick={()=>{onSearch(getRandomID())}}><FontAwesomeIcon icon={faShuffle} /></RandomButton>
             <SearchBar onSearch={onSearch}/>
         </NavContainer>
